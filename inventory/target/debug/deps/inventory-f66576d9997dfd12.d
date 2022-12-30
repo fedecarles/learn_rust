bb06914189f664dc56e6bc467f50184af44fb3ba
fedecarles <federico.carles@protonmail.com>
2022-12-30 18:29:48 -0300

    completed book

--- inventory/target/debug/deps/inventory-f66576d9997dfd12.d
+++
@@ -1,5 +0,0 @@
-/home/federico/Nextcloud/Projects/learn_rust/inventory/target/debug/deps/inventory-f66576d9997dfd12: src/main.rs
-
-/home/federico/Nextcloud/Projects/learn_rust/inventory/target/debug/deps/inventory-f66576d9997dfd12.d: src/main.rs
-
-src/main.rs:
--- inventory/target/debug/inventory.d
+++
@@ -1,1 +0,0 @@
-/home/federico/Nextcloud/Projects/learn_rust/inventory/target/debug/inventory: /home/federico/Nextcloud/Projects/learn_rust/inventory/src/main.rs
